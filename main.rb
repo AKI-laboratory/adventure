c198237179fb919adac3f972bb53f06d5cc44e51
Tanabe-Yumi <alleyoop58dank@gmail.com>
2021-12-04 15:43:34 +0900

    Edited main.rb

--- main.rb
+++ main.rb
@@ -1,9 +1,13 @@
 require 'dxopal'
+require_remote 'field.rb'
 include DXOpal
 Window.load_resources do
   Window.bgcolor = C_BLACK
 
+  field = Field.new
+
   Window.loop do
-    Window.draw_font(0, 0, "Hello!", Font.default, color: C_WHITE)
+    # Window.draw_font(0, 0, "Hello!", Font.default, color: C_WHITE)
+    field.drawField
   end
 end
