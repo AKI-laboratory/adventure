bc5a2e9a240d9c94776f93b61873d331d93e28f6
kktaia <65294191+kktaia@users.noreply.github.com>
2021-12-18 09:57:21 +0900

    Enemyを変更

--- source/enemy.rb
+++ source/enemy.rb
@@ -1,1 +1,27 @@
+require_remote 'source/chara.rb'
 
+class Enemy < Chara
+  attr_accessor :name
+
+  def initialize(x, y, image)
+    super(x, y, image)
+
+    @name = " "
+  end
+
+  def action
+
+  end
+
+  def die
+
+  end
+end
+
+class Zako1 < Enemy
+  def initialize(x, y, image)
+    super(x, y, image)
+    self.name = "雑魚１"
+    self.name.freeze
+  end
+end
